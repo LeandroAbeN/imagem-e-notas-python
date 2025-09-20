2353cfecdd30cf9080103041f03fe85336501d7a
LeandroAbeN <abe.leandro@gmail.com>
2025-09-20 03:16:20 -0300

    Update ImagemCinza.py

--- ImagemCinza.py
+++ ImagemCinza.py
@@ -1,17 +1,14 @@
 import numpy as np
-import cv2
 from skimage import io
 import matplotlib.pyplot as plt
 origem = "https://static.todamateria.com.br/upload/ap/er/apersistenciadamemoriasalvadordali1-cke.jpg"
 img_original = io.imread(origem)
-img_cinza = cv2.cvtColor(img_original, cv2.COLOR_RGB2GRAY)
-fig, ax = plt.subplots(1, 2,
-figsize=(10, 6))
+img_cinza = np.dot(img_original[...,:3], [0.2989, 0.5870, 0.1140])
+fig, ax = plt.subplots(1, 2, figsize=(10, 6))
 fig.tight_layout()
-ax[0].imshow(cv2.cvtColor(img_original,
-cv2.COLOR_BGR2RGB))
+ax[0].imshow(img_original)
 ax[0].set_title("Original")
-ax[1].imshow(cv2.cvtColor(img_cinza,
-cv2.COLOR_BGR2RGB))
+ax[1].imshow(img_cinza, cmap='gray')
 ax[1].set_title("Tons de Cinza")
+
 plt.show()
